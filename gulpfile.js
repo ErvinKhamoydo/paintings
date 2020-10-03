91f64d762809bf1abc50b65a05afa45319ffe684
Ervin <hamoydoerik@gmail.com>
2020-10-03 21:52:11 +0300

    Created the functionality of forms

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 const dist = "./dist/";
+// const dist = "../../../../../Programs/MAMP/htdocs/paintings/";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
