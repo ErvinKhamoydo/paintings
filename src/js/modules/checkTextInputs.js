fd3ca608a1ef8fd9d3ad24ae773dd0b92d09ca0e
Ervin <hamoydoerik@gmail.com>
2020-10-04 15:47:50 +0300

    Created a filter for the Russian language

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,19 @@
+const checkTextInputs = (selector) => {
+    const txtInputs = document.querySelectorAll(selector);
+
+    txtInputs.forEach(input => {
+        input.addEventListener('blur', () => {
+            if (input.value.match(/[^a-z]/ig)) {
+                input.value = '';
+            }
+        });
+
+        input.addEventListener('keypress', (e) => {
+            if (e.key.match(/[^а-яё 0-9]/ig)) {
+                e.preventDefault();
+            }
+        });
+    });
+};
+
+export default checkTextInputs;
